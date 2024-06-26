b36714081eec7d19402167b305a3f73e91b72d97
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-06-26 14:55:28 +0200

    feat(): serveur fonctionnel

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./User/route/auth'); // Assurez-vous que ce chemin est correct
+const authRoutes = require('./User/route/auth');
 
 dotenv.config();
 
@@ -16,7 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
 }).then(() => {
     console.log('Connected to MongoDB Atlas');
 }).catch((error) => {
@@ -24,6 +24,21 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 app.use('/auth', authRoutes);
+
+// Route pour la racine
+app.get('/', (req, res) => {
+    res.send('Welcome to the User Management API');
+});
+
+// Route pour la page de connexion
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'User/view/login.html'));
+});
+
+// Route pour la page de création de compte
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'User/view/register.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
