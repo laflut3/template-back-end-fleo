905579b76f994c3ff4c9a1b963e85b875fa09ed6
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-06-26 13:06:15 +0200

    feat(): back end file

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+// Charger les variables d'environnement
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
